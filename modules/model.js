1969540ddbd245bc0900f22b783a27d3453a44dc
seungkwon <yevgnenll@gmail.com>
2017-07-08 17:23:58 +0900

    contructor can have default value and print it

--- modules/model.js
+++ modules/model.js
@@ -1,6 +1,11 @@
 var Todo = Backbone.Model.extend({
+    'defaults': {
+        title: '',
+        completed: false,
+    },
     initialize: function() {
         console.log('this models has beean initailized.');
+        console.log('attr:', JSON.stringify(this));
     }
 });
 
